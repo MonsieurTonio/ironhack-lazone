b5169e468f3bc910adc27a4189551f659bc2ae3e
Antoine Ironhack <antoineironhack@MacBook-Pro-de-Antoine.local>
2019-01-29 19:24:01 +0100

    resolving conflict on routes/auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -24,30 +24,49 @@ router.get("/signup", (req, res, next) => {
 });
 
 router.post("/signup", (req, res, next) => {
-  const username = req.body.username;
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const company = req.body.company;
+  const email = req.body.email;
   const password = req.body.password;
-  if (username === "" || password === "") {
-    res.render("auth/signup", { message: "Indicate username and password" });
+  if (email === "" || password === "") {
+    res.render("auth/signup", { message: "Indicate email and password" });
     return;
   }
 
-  User.findOne({ username }, "username", (err, user) => {
+  User.findOne({ email }, "email", (err, user) => {
     if (user !== null) {
-      res.render("auth/signup", { message: "The username already exists" });
+      res.render("auth/signup", { message: "The email already exists" });
       return;
     }
 
     const salt = bcrypt.genSaltSync(bcryptSalt);
     const hashPass = bcrypt.hashSync(password, salt);
 
+
+    const email = req.body.email;
+    const characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let token = '';
+    for (let i = 0; i < 25; i++) {
+      token += characters[Math.floor(Math.random() * characters.length)];
+    }
+
+    const confirmationUrl = `http://localhost:3000/auth/confirm/${newUser.confirmationCode}`
+
+
     const newUser = new User({
-      username,
-      password: hashPass
+      firstName,
+      lastName,
+      company,
+      email,
+      password: hashPass,
+      confirmationCode: token
     });
 
     newUser.save()
     .then(() => {
-      res.redirect("/");
+      console.log("dashboard ok")
+      res.redirect("/dashboard");
     })
     .catch(err => {
       res.render("auth/signup", { message: "Something went wrong" });
