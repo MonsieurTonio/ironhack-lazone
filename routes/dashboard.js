4130a8dbe5806270e2f9ef679f94b48171ff01e7
Nicolas Prieur <nicolas.prieurdelacomble@gmail.com>
2019-02-04 20:01:26 +0100

    last update

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -7,7 +7,8 @@ const User = require("../models/User");
 router.get('/', (req, res, next) => {
   User.findById(req.user._id, (err, user) => {
 
-  res.render('dashboard');
+    res.render('dashboard');
+  });
 });
 
 
