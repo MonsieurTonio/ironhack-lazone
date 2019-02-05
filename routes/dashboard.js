e4abbc97c443a4dbf8f6e1d9a1082ea9d83acbca
MonsieurTonio <admtoine@gmail.com>
2019-02-05 22:41:04 +0100

    search by artist name

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,15 +3,42 @@ const passport = require('passport');
 const router = express.Router();
 const User = require("../models/User");
 const ensureLogin = require("connect-ensure-login");
+const SpotifyWebApi = require('spotify-web-api-node');
 
+// Remember to insert your credentials here
+const spotifyApi = new SpotifyWebApi({
+  clientId : process.env.spotifyClientId,
+  clientSecret : process.env.spotifyClientSecret
+ });
+ 
+ // Retrieve an access token
+ spotifyApi.clientCredentialsGrant()
+   .then( data => {
+     spotifyApi.setAccessToken(data.body['access_token']);
+   })
+   .catch(error => {
+     console.log('Something went wrong when retrieving an access token', error);
+   })
 
 router.get('/', ensureLogin.ensureLoggedIn(), (req, res, next) => {
+  console.log('hey')
   User.findById(req.user._id, (err, user) => {
     if (err) return next(err);
 
     res.render('dashboard', { user: user });
   })
 });
+
+router.post('/', (req, res, next) => {
+console.log(req.body.artist)
+spotifyApi.searchArtists(req.body.artist)
+.then(data => {
+          res.render('dashboard',{artists:data.body.artists.items});
+       })
+       .catch(err => {
+         console.log("error");
+       })
+      })
 
 
 module.exports = router;
