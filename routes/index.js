04d7f85f370d8515a3083646178cd17b4fc9445c
Nicolas Prieur <nicolas.prieurdelacomble@gmail.com>
2019-02-07 21:27:31 +0100

    clean profile page

--- routes/index.js
+++ routes/index.js
@@ -15,8 +15,6 @@ router.get('/', (req, res, next) => {
 
 router.get('/profile', /*ensureLogin.ensureLoggedIn(),*/ (req, res, next) => {
 
-  console.log('coucouu')
-
     User.findById(req.user._id,function(err,user){
       console.log("hey");
       if (err) return next(err);
@@ -30,25 +28,33 @@ router.get('/profile', /*ensureLogin.ensureLoggedIn(),*/ (req, res, next) => {
 });
 
 router.post('/profile', /*ensureLogin.ensureLoggedIn(),*/ uploadCloud.single('photo'), (req, res, next) => {
-  console.log('coucpu') 
 
   User.findById(req.user._id,function(err,user){
     if (err) return next(err);
 
     if (req.file) {
-      user.avatarUrl = req.file.url
+      user.avatarUrl = req.file.url;
     }
 
-    user.firstName = req.body.firstName;
-    user.lastName = req.body.lastName;
-    user.company = req.body.company;
-    user.email= req.body.email;
+    if (user.firstName){
+      user.firstName = req.body.firstName;
+    }
+
+    if (user.lastName){
+      user.lastName = req.body.lastName;
+    }
+
+    if (user.company){
+      user.company = req.body.company;
+    }
+
+    if (user.email){
+      user.email = req.body.email;
+    }
 
     user.save(function(err, user){
       if (err) return next(err);
-
-      res.send('/');
-      console.log("yes");
+      res.render('profile');
     });
 
 
