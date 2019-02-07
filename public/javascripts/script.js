bc13fc848095195ec03ba15a554d572dfa5f8939
MonsieurTonio <admtoine@gmail.com>
2019-02-07 19:57:17 +0100

    call Spotify API to search an artist ok

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,3 +3,13 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('IronGenerator JS imported successfully!');
 
 }, false);
+
+
+
+
+const selectedArtist = []
+
+$("#selectArtist").click(function () {
+  target = event.target.innerHTML;
+  selectedArtist.push(target)
+});
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -4,41 +4,69 @@ const router = express.Router();
 const User = require("../models/User");
 const ensureLogin = require("connect-ensure-login");
 const SpotifyWebApi = require('spotify-web-api-node');
+const document = require('../views')
 
 // Remember to insert your credentials here
 const spotifyApi = new SpotifyWebApi({
-  clientId : process.env.spotifyClientId,
-  clientSecret : process.env.spotifyClientSecret
- });
- 
- // Retrieve an access token
- spotifyApi.clientCredentialsGrant()
-   .then( data => {
-     spotifyApi.setAccessToken(data.body['access_token']);
-   })
-   .catch(error => {
-     console.log('Something went wrong when retrieving an access token', error);
-   })
-
-router.get('/', ensureLogin.ensureLoggedIn(), (req, res, next) => {
-  console.log('hey')
-  User.findById(req.user._id, (err, user) => {
-    if (err) return next(err);
-
-    res.render('dashboard', { user: user });
-  })
+  clientId: process.env.spotifyClientId,
+  clientSecret: process.env.spotifyClientSecret
 });
 
-router.post('/', (req, res, next) => {
-console.log(req.body.artist)
-spotifyApi.searchArtists(req.body.artist)
-.then(data => {
-          res.render('dashboard',{artists:data.body.artists.items});
-       })
-       .catch(err => {
-         console.log("error");
-       })
+// Retrieve an access token
+spotifyApi.clientCredentialsGrant()
+  .then(data => {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  })
+  .catch(error => {
+    console.log('Something went wrong when retrieving an access token', error);
+  })
+
+
+
+router.get('/', ensureLogin.ensureLoggedIn(), (req, res, next) => {
+  console.log('hey');
+
+  //ICI rajouter les artistes déjà saved dans ma database
+
+  User.find(req.user._id)
+    .catch(err => next(err))
+    .then((dbartists) => {
+      
+  })
+
+  // })
+
+  // let dbartists = [];
+
+  if (req.query.artist) {
+    spotifyApi.searchArtists(req.query.artist)
+      .catch(err => {
+        return next(err); //Si erreur => on arrêt tout et on veut afficher l'erreur
+      })  
+      .then((data) => {
+        res.render('dashboard', {
+          artistsfound: data.body.artists.items,
+          queryname: req.query.artist,
+          myartists: dbartists
+        });
       })
+    ;
+      
+  } else {
+    res.render('dashboard', {
+      myartists: dbartists
+    });
+  }
+});
+
+
+
+
+
+
+router.post('/', (req, res, next) => {
+  res.send('post ok') // on envoie "ok" pour répondre à la requête
+});
 
 
 module.exports = router;
