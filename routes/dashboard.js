d43d5546c313ce64bcd5361ec087f4347c34a26c
MonsieurTonio <admtoine@gmail.com>
2019-02-07 22:09:46 +0100

    add newArtist to db

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -5,6 +5,8 @@ const User = require("../models/User");
 const ensureLogin = require("connect-ensure-login");
 const SpotifyWebApi = require('spotify-web-api-node');
 const document = require('../views')
+const Artist = require("../models/Artist")
+
 
 // Remember to insert your credentials here
 const spotifyApi = new SpotifyWebApi({
@@ -19,56 +21,76 @@ spotifyApi.clientCredentialsGrant()
   })
   .catch(error => {
     console.log('Something went wrong when retrieving an access token', error);
-  })
+  });
 
 
 
 router.get('/', ensureLogin.ensureLoggedIn(), (req, res, next) => {
   console.log('hey');
 
-
-
   // User.findById(req.user._id) ---> On n'utilise pas le User.findById car il est déjà dans le req.user grâce au ensureLogin
   //   .catch(err => next(err))
   //   .then((user) => {
-
   //   })
   // ;
-      
 
-  // ICI rajouter les artistes déjà saved dans ma database
+  // Ici rajouter les artistes déjà saved dans ma database
 
-    let dbartists = req.user.artistsFollowed
+  let dbartists = req.user.artistsFollowed
 
-    if (req.query.artist) {
-      spotifyApi.searchArtists(req.query.artist)
-        .catch(err => {
-          return next(err); //Si erreur => on arrêt tout et on veut afficher l'erreur
-        })  
-        .then((data) => {
-          res.render('dashboard', {
-            artistsfound: data.body.artists.items,
-            queryname: req.query.artist,
-            myartists: dbartists
-          });
-        })
+  if (req.query.artist) {
+    spotifyApi.searchArtists(req.query.artist)
+      .catch(err => {
+        return next(err); //Si erreur => on arrêt tout et on veut afficher l'erreur
+      })
+      .then((data) => {
+        res.render('dashboard', {
+          artistsfound: data.body.artists.items,
+          queryname: req.query.artist,
+          myartists: dbartists
+        });
+      })
       ;
-    } else {
-      res.render('dashboard', {
-        myartists: dbartists
-      });
-    }
+  } else {
+    res.render('dashboard', {
+      myartists: dbartists
+    });
+  }
 
 
-  })
+})
 
-  // })
+// })
 
 
 
 router.post('/', (req, res, next) => {
-  res.send('post ok') // on envoie "ok" pour répondre à la requête
-});
+  spotifyApi.getArtist(req.body.artistid)
+  spotifyApi.getArtist(req.body.artistid)
+    .catch(err => {
+      return next(err); //Si erreur => on arrêt tout et on veut afficher l'erreur
+    })
+    .then((data) => {
+      console.log(data.body.name);
+      var newArtist = new Artist({
+        artistName: data.body.name,
+        spotifyAccountId: data.body.id,
+        genre: data.body.genres,
+        album: data.body.albums,
+        datas: [data.body.followers]
+      })
+
+      newArtist.save(function (err) {
+        if (err) {
+          throw err;
+        } else {
+          console.log('Save artist successfully!');
+        }
+      })
+    })
+
+      res.render('dashboard') // on envoie le dashboard pour répondre à la requête
+  });
 
 
 module.exports = router;
