7b096e755daa44c6695e9883b6381fe327ef2054
Nicolas Prieur <nicolas.prieurdelacomble@gmail.com>
2019-02-07 20:28:16 +0100

    fix cloudinary and retrieve profile inputs to mongodb

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+  const express = require('express');
 const multer  = require('multer');
 const router  = express.Router();
 const uploadCloud = require('../config/cloudinary.js');
@@ -18,6 +18,7 @@ router.get('/profile', /*ensureLogin.ensureLoggedIn(),*/ (req, res, next) => {
   console.log('coucouu')
 
     User.findById(req.user._id,function(err,user){
+      console.log("hey");
       if (err) return next(err);
 
       console.log('user', user)
@@ -29,15 +30,27 @@ router.get('/profile', /*ensureLogin.ensureLoggedIn(),*/ (req, res, next) => {
 });
 
 router.post('/profile', /*ensureLogin.ensureLoggedIn(),*/ uploadCloud.single('photo'), (req, res, next) => {
-  console.log('coucpu', req.file.url) 
+  console.log('coucpu') 
 
-  User.findById("5c4c7b50e36d9013b2fec8b0",function(err,user){
+  User.findById(req.user._id,function(err,user){
     if (err) return next(err);
-    user.avatarUrl = req.file.url
-    user.save(function(err,user){
+
+    if (req.file) {
+      user.avatarUrl = req.file.url
+    }
+
+    user.firstName = req.body.firstName;
+    user.lastName = req.body.lastName;
+    user.company = req.body.company;
+    user.email= req.body.email;
+
+    user.save(function(err, user){
       if (err) return next(err);
-      res.send('update ok')
+
+      res.send('/');
+      console.log("yes");
     });
+
 
   });
   
