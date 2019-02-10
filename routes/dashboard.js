c3d2cc69b78d4cc6e9ad36fbcd940685a8a42c7c
Arthur Houdebine <arthur.houdebine@gmail.com>
2019-02-10 16:55:52 +0100

    fixed artists followed

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -48,60 +48,71 @@ router.get('/', ensureLogin.ensureLoggedIn(), (req, res, next) => {
               queryname: req.query.artist,
               myartists: dbartists
             });
-          })
-          ;
+          });
       } else {
         res.render('dashboard', {
           user: user,
           myartists: dbartists
         });
       }
-    })
-  ;
+    });
 })
 
 
 router.post('/', (req, res, next) => {
-
   Artist.findOne({
       spotifyAccountId: req.body.artistid
     })
     .then((artist) => {
       if (!artist) {
+// User.findById(req.user._id) // ---> On n'utilise pas le User.findById car il est déjà dans le req.user grâce au ensureLogin
+// .catch(err => next(err))
+// .then((user) => {
+// user.artistsFollowed.push(artist.body.id)
+// user.save()
+// console.log("artist sauvé dans artistfollowed");
+// })
+
         spotifyApi.getArtist(req.body.artistid) //on chope les données de l'artiste
           .catch(err => {
             return next(err); //Si erreur => on arrêt tout et on veut afficher l'erreur
           })
-          .then(artist => { 
+          .then(artist => {
               spotifyApi.getArtistTopTracks(req.body.artistid, 'FR') //on chope les top tracks
-            .then(toptracks => {
-              console.log(toptracks);
-              console.log(artist);
-              var newArtist = new Artist({
-                artistName: artist.body.name,
-                spotifyAccountId: artist.body.id,
-                genre: artist.body.genres,
-                album: artist.body.albums,
-                image: artist.body.images[0].url,
-                toptracks:[toptracks.body.tracks[0].name,toptracks.body.tracks[1].name,toptracks.body.tracks[2].name],
-                datas: [{
-                  spotifyFlws: artist.body.followers.total,
-                  spotifyPopularityScore: artist.body.popularity
-                }]
-              })
+                .then(toptracks => {
+                  console.log(toptracks);
+                  console.log(artist);
+                  var newArtist = new Artist({
+                    artistName: artist.body.name,
+                    spotifyAccountId: artist.body.id,
+                    genre: artist.body.genres,
+                    album: artist.body.albums,
+                    image: artist.body.images[0].url,
+                    toptracks: [toptracks.body.tracks[0].name, toptracks.body.tracks[1].name, toptracks.body.tracks[2].name],
+                    datas: [{
+                      spotifyFlws: artist.body.followers.total,
+                      spotifyPopularityScore: artist.body.popularity
+                    }]
+                  })
 
-              newArtist.save(function (err) {
-                if (err) {
-                  return next(err);
-                } else {
-                  console.log('Save artist successfully!');
-                }
-              })
-              res.render('dashboard') //on envoie le dashboard pour répondre à la requête
-            })
-          }
+                  newArtist.save(function (err) {
+                    if (err) {
+                      return next(err);
+                    } else {
+                      console.log('Save artist successfully!');
+                    }
+                  })
+                  User.findById(req.user._id,function(err,user){
+                    if (err) return next(err);
+                    user.artistsFollowed.push(newArtist._id)
+                    console.log("artiste sauvé dans artist followed!")
+                    user.save();
+                  })
+                  res.render('dashboard') //on envoie le dashboard pour répondre à la requête
+                })
+            }
 
-        )
+          )
 
 
       } else {
